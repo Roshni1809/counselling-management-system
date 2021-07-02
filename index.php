838202d9449763d52027683742797cdfcfe329c0
Roshni Shukla <71212064+Roshni1809@users.noreply.github.com>
2021-07-02 20:21:43 +0530

    .

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+
+        <?php
+        ini_set( "display_errors", 0); 
+        session_start();
+        session_destroy();
+        include 'index.html';
+        // put your code here
+        ?>
+  
